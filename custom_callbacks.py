7f8ca3fd474fa0b7334fe4f9ab644dbfa3992524
SebChw <sebastian.chwilczynski@gmail.com>
2023-01-19 09:15:43 +0100

    add: formatting

--- custom_callbacks.py
+++ custom_callbacks.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from utils import cut_tensor_from_0_to_1
 """All callbacks here assume that logger is a neptune one"""
 
+
 class ImageLoggingCallback(Callback):
     def __init__(self,):
         super().__init__()
@@ -14,27 +15,29 @@ class ImageLoggingCallback(Callback):
 
     def on_validation_epoch_end(self, trainer, pl_module):
         # We always take the same photo and take only one so that neptun doesn't cry
-        validset = SRDataset(train=False, return_scaling_factor=False, perform_bicubic=True) #Not the most efficient solution
+        # Not the most efficient solution
+        validset = SRDataset(
+            train=False, return_scaling_factor=False, perform_bicubic=True)
         lr, hr = validset[2]
 
-        lr_to_hr = pl_module(torch.unsqueeze(lr, 0).to("cuda"))[0] # making it batch of size 1, it must be on the same device as model
+        # making it batch of size 1, it must be on the same device as model
+        lr_to_hr = pl_module(torch.unsqueeze(lr, 0).to("cuda"))[0]
 
-        lr_filled = torch.zeros((3, lr.shape[1], hr.shape[2]))# it is probably the other way around as in opencv
-        lr_filled[:,:,:hr.shape[2]] = lr
+        # it is probably the other way around as in opencv
+        lr_filled = torch.zeros((3, lr.shape[1], hr.shape[2]))
+        lr_filled[:, :, :hr.shape[2]] = lr
 
         lr_to_hr = cut_tensor_from_0_to_1(lr_to_hr)
 
-        grid = torchvision.utils.make_grid(torch.cat([lr_filled, lr_to_hr.to("cpu"), hr],axis=2)) #lr_to_hr must be on the same device as the rest
-        
-        #TODO Save the images in some logging folder
+        grid = torchvision.utils.make_grid(torch.cat([lr_filled, lr_to_hr.to(
+            "cpu"), hr], axis=2))  # lr_to_hr must be on the same device as the rest
+
+        # TODO Save the images in some logging folder
         img_path = f"img{self.img_num}.png"
         torchvision.utils.save_image(grid, img_path)
-        pl_module.logger.experiment[f"validation/results{self.img_num}"].upload(img_path)
-        self.img_num +=1
+        pl_module.logger.experiment[f"validation/results{self.img_num}"].upload(
+            img_path)
+        self.img_num += 1
 
         #! If i remove this image here it won't be sent to neptune :(
-        #Path(img_path).unlink()
-
-
-
-        
+        # Path(img_path).unlink()
